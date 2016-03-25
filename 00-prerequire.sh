1d8a74789aaa2e13b5731364da9391810ab10c79
Van Duc Uy <vducuy@gmail.com>
2016-03-25 12:05:23 +0700

    auto accept

--- 00-prerequire.sh
+++ 00-prerequire.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-apt-get install chrony
+apt-get install -y chrony
 service chrony restart
 chronyc sources
 
 #Enable the OpenStack repository
-apt-get install software-properties-common
+apt-get install -y software-properties-common
 add-apt-repository cloud-archive:liberty
 
 #Finalize the installation
 apt-get update && apt-get dist-upgrade
 
 #Install the OpenStack client:
-apt-get install python-openstackclient
+apt-get install -y python-openstackclient
 
 
-apt-get install mariadb-server python-pymysql
+apt-get install -y mariadb-server python-pymysql
 
