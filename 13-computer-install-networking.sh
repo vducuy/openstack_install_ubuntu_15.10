7426216d266df1929bbb3c126f911a9da0d55ae9
Van Duc Uy <vducuy@gmail.com>
2016-03-30 13:49:19 +0700

    Add networking on Computer node

---
+++ 13-computer-install-networking.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "#Install networking"
+apt-get install neutron-plugin-linuxbridge-agent -y
+echo "#Edit the /etc/neutron/neutron.conf file and complete the following actions:"
+echo "#In the [database] section, comment out any connection options because compute nodes do not directly access the database."
+crudini --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_host controller
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_userid openstack
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_password amcc1234
+crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --del /etc/neutron/neutron.conf keystone_authtoken
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_uri http://controller:5000
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url http://controller:35357
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_plugin password
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken user_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
+crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
+crudini --set /etc/neutron/neutron.conf keystone_authtoken password amcc1234
+crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
+echo "#Config linux bridege"
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini linux_bridge physical_interface_mappings public:eth1
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan enable_vxlan True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan local_ip 10.38.70.21
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan l2_population True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini agent prevent_arp_spoofing True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup enable_security_group True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
+echo "#Configure compute to use the network"
+crudini --set /etc/nova/nova.conf neutron url http://controller:9696
+crudini --set /etc/nova/nova.conf neutron auth_url http://controller:35357
+crudini --set /etc/nova/nova.conf neutron auth_plugin password
+crudini --set /etc/nova/nova.conf neutron project_domain_id default
+crudini --set /etc/nova/nova.conf neutron user_domain_id default
+crudini --set /etc/nova/nova.conf neutron region_name RegionOne
+crudini --set /etc/nova/nova.conf neutron project_name service
+crudini --set /etc/nova/nova.conf neutron username neutron
+crudini --set /etc/nova/nova.conf neutron password amcc1234
+service nova-compute restart
+service neutron-plugin-linuxbridge-agent restart
+
