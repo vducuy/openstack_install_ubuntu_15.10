7d00d0203ac69e6a87c919d13b72a3e083544f27
Van Duc Uy <vducuy@gmail.com>
2016-04-28 17:19:47 +0700

    update keystone install

--- 03-controller-install-keystone.sh
+++ 03-controller-install-keystone.sh
@@ -12,16 +12,15 @@ export ADMIN_TOKEN=$(openssl rand -hex 10)
 #Disable the keystone service from starting automatically after installation
 echo "manual" > /etc/init/keystone.override
 #Run the following command to install the packages
-apt-get install keystone apache2 libapache2-mod-wsgi \
-  memcached python-memcache -y
+apt-get install keystone apache2 libapache2-mod-wsgi -y
 #Edit the /etc/keystone/keystone.conf file and complete the following actions
 crudini --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
 crudini --set /etc/keystone/keystone.conf DEFAULT verbose True
 crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:${ADMIN_PASSWORD}@controller/keystone
 crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
-crudini --set /etc/keystone/keystone.conf token provider uuid
-crudini --set /etc/keystone/keystone.conf token driver memcache
-crudini --set /etc/keystone/keystone.conf revoke driver sql
+crudini --set /etc/keystone/keystone.conf token provider fernet
+#crudini --set /etc/keystone/keystone.conf token driver memcache
+#crudini --set /etc/keystone/keystone.conf revoke driver sql
 #Populate the Identity service database:
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 #Configure the Apache HTTP server
@@ -29,6 +28,8 @@ sed -i '14 a ServerName controller' /etc/apache2/apache2.conf
 cp wsgi-keystone.conf /etc/apache2/sites-available/wsgi-keystone.conf
 #Enable the Identity service virtual hosts
 ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
+# initial Fernet key
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 #Finalize the installation
 service apache2 restart
 
